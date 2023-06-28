16ee4dfdf95e4ec25ee14e60fbd18bd1a0acb876
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-06-28 08:08:10 -0500

    Adding POJO's

---
+++ src/main/java/com/spring/petclinic/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.spring.petclinic.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/spring/petclinic/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.spring.petclinic.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
