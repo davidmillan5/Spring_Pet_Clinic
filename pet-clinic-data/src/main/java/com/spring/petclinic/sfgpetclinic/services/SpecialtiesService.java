6697d5fa15443e92d3efa94082c91b887ef51e43
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-08-02 23:21:12 -0500

    Added Specialities Map Impl. Closes #32

---
+++ pet-clinic-data/src/main/java/com/spring/petclinic/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.spring.petclinic.sfgpetclinic.services;
+
+import com.spring.petclinic.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long> {
+}
