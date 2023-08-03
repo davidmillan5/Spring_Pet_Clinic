04bf94eaff14b3d4dcfef88e3683b31e468de1ab
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-08-02 22:21:47 -0500

    added PetTypeService. closes #30

---
+++ pet-clinic-data/src/main/java/com/spring/petclinic/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.spring.petclinic.sfgpetclinic.services;
+
+import com.spring.petclinic.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
